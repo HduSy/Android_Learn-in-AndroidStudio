6bc3dd6918f3fd2508f2a4733a540a0ca79a396c
hdusy <2817679579@qq.com>
2019-05-24 20:49:21 +0800

    Initial commit

---
+++ app/src/main/java/com/example/tarmac/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.tarmac;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+	int[] images = {};
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
