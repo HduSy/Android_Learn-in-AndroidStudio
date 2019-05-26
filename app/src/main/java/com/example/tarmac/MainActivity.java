76a9ac85914fe8db202bdf63943a0e0dd4a0ec26
hdusy <2817679579@qq.com>
2019-05-26 21:51:12 +0800

    AdapterView及其子类

--- app/src/main/java/com/example/tarmac/MainActivity.java
+++ app/src/main/java/com/example/tarmac/MainActivity.java
@@ -3,14 +3,99 @@ package com.example.tarmac;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterViewFlipper;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-	int[] images = {};
+	private String[] titles = {
+			"List01",
+			"List02",
+			"List03",
+			"List04",
+			"List05",
+			"List06",
+			"List07",
+			"List08",
+			"List09",
+			"List10",
+			"List11",
+			"List12"
+	};
+	private String[] descs = {
+			"FUCK0101010101",
+			"FUCK0202020202",
+			"FUCK0303030303",
+			"FUCK0404040404",
+			"FUCK0505050505",
+			"FUCK0606060606",
+			"FUCK0707707077",
+			"FUCK8888888888",
+			"FUCK9999999999",
+			"FUCK1010101010",
+			"FUCK1111111111",
+			"FUCK1212121212"
+
+	};
+	private int[] imgs = {
+			R.drawable.w1,
+			R.drawable.w2,
+			R.drawable.w3,
+			R.drawable.w4,
+			R.drawable.w5,
+			R.drawable.w6,
+			R.drawable.w7,
+			R.drawable.w8,
+			R.drawable.w9,
+			R.drawable.w10,
+			R.drawable.w11,
+			R.drawable.w12
+	};
+	AdapterViewFlipper flipper;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		flipper = (AdapterViewFlipper)findViewById(R.id.af);
+		BaseAdapter baseAdapter = new BaseAdapter() {
+			@Override
+			public int getCount() {
+				return imgs.length;
+			}
+
+			@Override
+			public Object getItem(int position) {
+				return imgs[position];
+			}
+
+			@Override
+			public long getItemId(int position) {
+				return position;
+			}
+
+			@Override
+			public View getView(int position, View convertView, ViewGroup parent) {
+				ImageView imageView = new ImageView(MainActivity.this);
+				imageView.setImageResource(imgs[position]);
+				imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+				imageView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+				return imageView;
+			}
+		};
+		flipper.setAdapter(baseAdapter);
+	}
+	public void prev(View view) {
+		flipper.showPrevious();
+		flipper.stopFlipping();
+	}
+
+	public void next(View view) {
+		flipper.showNext();
+		flipper.stopFlipping();
+	}
+
+	public void auto(View view) {
+		flipper.startFlipping();
 	}
 }
