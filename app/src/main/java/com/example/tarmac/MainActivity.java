18e0c9d8c6704d91f419ef3e04a20dbf9d1db927
hdusy <2817679579@qq.com>
2019-06-16 17:31:00 +0800

    19.6.16

--- app/src/main/java/com/example/tarmac/MainActivity.java
+++ app/src/main/java/com/example/tarmac/MainActivity.java
@@ -1,102 +1,78 @@
 package com.example.tarmac;
 
-import android.app.Activity;
-import android.app.DatePickerDialog;
-import android.app.TimePickerDialog;
+import android.app.ProgressDialog;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.DatePicker;
-import android.widget.EditText;
-import android.widget.PopupWindow;
-import android.widget.TimePicker;
-import android.widget.Toast;
-
-import java.util.Calendar;
-import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
-	private String[] titles = {
-			"List01",
-			"List02",
-			"List03",
-			"List04",
-			"List05",
-			"List06",
-			"List07",
-			"List08",
-			"List09",
-			"List10",
-			"List11",
-			"List12"
-	};
-	private String[] descs = {
-			"FUCK0101010101",
-			"FUCK0202020202",
-			"FUCK0303030303",
-			"FUCK0404040404",
-			"FUCK0505050505",
-			"FUCK0606060606",
-			"FUCK0707707077",
-			"FUCK8888888888",
-			"FUCK9999999999",
-			"FUCK1010101010",
-			"FUCK1111111111",
-			"FUCK1212121212"
-
-	};
-	private int[] imgs = {
-			R.drawable.w1,
-			R.drawable.w2,
-			R.drawable.w3,
-			R.drawable.w4,
-			R.drawable.w5,
-			R.drawable.w6,
-			R.drawable.w7,
-			R.drawable.w8,
-			R.drawable.w9,
-			R.drawable.w10,
-			R.drawable.w11,
-			R.drawable.w12
-	};
-	private String books[] = {
-			"《Java疯狂讲义》",
-			"《Android疯狂讲义》",
-			"《C++ Primer plus》"
+	final static int MAX_STATUS = 100;
+	private int[] data = new int[50];
+	int progressStatus = 0;
+	int hasData = 0;
+	ProgressDialog pd1, pd2;
+	//	更新进度的handler
+	Handler handler = new Handler() {
+		@Override
+		public void handleMessage(Message msg) {
+			if (msg.what == 0x123) {
+				pd2.setProgress(progressStatus);
+			}
+		}
 	};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		Button dateDialog = (Button)findViewById(R.id.dateDialog);
-		Button timeDialog = (Button)findViewById(R.id.timeDialog);
-		dateDialog.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Calendar c = Calendar.getInstance();
-				new DatePickerDialog(MainActivity.this,new DatePickerDialog.OnDateSetListener(){
-					@Override
-					public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-						Toast.makeText(MainActivity.this, "您选择了"+year+"年"+month+"月"+dayOfMonth+"日", Toast.LENGTH_SHORT).show();
-					}
-				},c.get(Calendar.YEAR),c.get(Calendar.MONTH),c.get(Calendar.DAY_OF_MONTH)).show();
-			}
-		});
-		timeDialog.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Calendar c = Calendar.getInstance();
-				new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
-					@Override
-					public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-						Toast.makeText(MainActivity.this, "您选择了"+hourOfDay+"时"+minute+"分",Toast.LENGTH_SHORT ).show();
-					}
-				},c.get(Calendar.HOUR_OF_DAY),c.get(Calendar.MINUTE),true).show();
-			}
-		});
+		setContentView(R.layout.learn_layout);
 	}
 
+	public void showSpinner(View src) {
+		ProgressDialog.show(this, "任务执行中", "任务执行中，请等待", false, true);
+	}
+
+	public void showIndeterminate(View src) {
+		pd1 = new ProgressDialog(MainActivity.this);
+		pd1.setTitle("任务执行中");
+		pd1.setMessage("任务执行中，请耐心等待");
+		pd1.setIndeterminate(false);
+		pd1.setCancelable(true);
+		pd1.show();
+	}
+
+	public void showProgress(View src) {
+		progressStatus = 0;
+		hasData = 0;
+		pd2 = new ProgressDialog(MainActivity.this);
+		pd2.setMax(MAX_STATUS);
+		pd2.setTitle("任务完成百分比");
+		pd2.setMessage("耗时任务的完成百分比");
+		pd2.setCancelable(true);
+		pd2.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+		pd2.setIndeterminate(false);
+		pd2.show();
+		new Thread() {
+			@Override
+			public void run() {
+				while (progressStatus < MAX_STATUS) {
+					progressStatus = MAX_STATUS * doWork() / data.length;
+					handler.sendEmptyMessage(0x123);
+				}
+				pd2.dismiss();
+			}
+		}.start();
+	}
+
+	public int doWork() {
+		data[hasData++] = (int) (Math.random() * 100);
+		try {
+			Thread.sleep(10);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		return hasData;
+	}
 }
